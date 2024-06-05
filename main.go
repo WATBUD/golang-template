03f01be2503234476ac24a9a92e47299a6a2c4f8
水靈 <z23320785@gmail.com>
2024-06-05 15:03:42 +0800

    Reorganizing Project Architecture

--- main.go
+++ main.go
@@ -1,69 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-type Item struct {
-	ID    string `json:"id,omitempty"`
-	Name  string `json:"name,omitempty"`
-	Price int    `json:"price,omitempty"`
-}
-
-var items []Item
-
-func GetItems(w http.ResponseWriter, r *http.Request) {
-	items = []Item{
-		{ID: "1", Name: "Item 1", Price: 10},
-		{ID: "2", Name: "Item 2", Price: 20},
-		{ID: "3", Name: "Item 3", Price: 30},
-		{ID: "4", Name: "Item 4", Price: 40},
-	}
-	json.NewEncoder(w).Encode(items)
-}
-
-func GetItem(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range items {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Item{})
-}
-
-func CreateItem(w http.ResponseWriter, r *http.Request) {
-	var item Item
-	_ = json.NewDecoder(r.Body).Decode(&item)
-	items = append(items, item)
-	json.NewEncoder(w).Encode(items)
-}
-
-func DeleteItem(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range items {
-		if item.ID == params["id"] {
-			items = append(items[:index], items[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(items)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-	router := mux.NewRouter()
-
-	// 添加路由处理函数
-	router.HandleFunc("/items", GetItems).Methods("GET")
-	router.HandleFunc("/items/{id}", GetItem).Methods("GET")
-	router.HandleFunc("/items", CreateItem).Methods("POST")
-	router.HandleFunc("/items/{id}", DeleteItem).Methods("DELETE")
-
-	// 启动服务器
-	log.Fatal(http.ListenAndServe(":8000", router))
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
