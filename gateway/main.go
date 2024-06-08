3895f9ac3c8766a6dec967b93f5e06f9a5c182bf
水靈 <z23320785@gmail.com>
2024-06-08 21:28:20 +0800

    update deploy_cloud_run FILE

--- gateway/main.go
+++ gateway/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!gatewaygatewaygatewaygateway")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!-gateway-gateway")
+	})
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
